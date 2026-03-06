e38dd54a4a6770f436162f63d55d70195c547177
sravs2004 <neruturisravani.3472@gmail.com>
2026-03-06 14:43:31 +0530

    fix render port issue

--- app.py
+++ app.py
@@ -293,9 +293,6 @@ def dashboard():
         weak_topics=weak_topics
     )
 
-
 if __name__ == "__main__":
-
-    port = int(os.environ.get("PORT",10000))
-
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
